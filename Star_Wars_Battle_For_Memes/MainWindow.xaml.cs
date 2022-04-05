c32e908b47e10e0731efc9684d704da0b22a104b
julidow <j.dowsilas@gmail.com>
2022-04-05 18:24:04 +0200

    Configuring player

--- Star_Wars_Battle_For_Memes/MainWindow.xaml.cs
+++ Star_Wars_Battle_For_Memes/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Star_Wars_Battle_For_Memes
 {
@@ -20,9 +21,35 @@ namespace Star_Wars_Battle_For_Memes
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        #region variables
+        DispatcherTimer gameTimer = new DispatcherTimer(); //game main timer
+        Random random = new Random();
+
+        bool moveLeft, moveRight, moveUp, moveDown; //player controls
+        Rect playerHitBox;
+
+        #endregion
+
         public MainWindow()
         {
             InitializeComponent();
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            gameTimer.Tick += GameLoop;
+
+            GameCanvas.Focus();
+
+            //background
+            ImageBrush bg = new ImageBrush();
+            bg.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/background.png"));
+            bg.TileMode = TileMode.Tile;
+            bg.Viewport = new Rect(0, 0, 0.15, 0.15);
+            bg.ViewportUnits = BrushMappingMode.RelativeToBoundingBox;
+            GameCanvas.Background = bg;
+        }
+        private void GameLoop(object sender, EventArgs e) //main game loop
+        {
+
         }
 
 
@@ -34,11 +61,19 @@ namespace Star_Wars_Battle_For_Memes
 
             if (e.Key == Key.Left)
             {
-                //moveLeft = true;
+                moveLeft = true;
             }
             if (e.Key == Key.Right)
             {
-                //moveRight = true;
+                moveRight = true;
+            }
+            if (e.Key == Key.Up)
+            {
+                moveUp = true;
+            }
+            if (e.Key == Key.Down)
+            {
+                moveDown = true;
             }
 
         }
@@ -46,11 +81,19 @@ namespace Star_Wars_Battle_For_Memes
         {
             if (e.Key == Key.Left)
             {
-                //moveLeft = false;
+                moveLeft = false;
             }
             if (e.Key == Key.Right)
             {
-                //moveRight = false;
+                moveRight = false;
+            }
+            if (e.Key == Key.Up)
+            {
+                moveUp = false;
+            }
+            if (e.Key == Key.Down)
+            {
+                moveDown = false;
             }
         }
     }
